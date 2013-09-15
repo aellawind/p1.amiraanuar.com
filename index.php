e75ad628f9d7cd09835a64f7b9c6109e3e63ce0f
Amira Anuar <amiraanuar@gmail.com>
2013-09-15 01:19:20 -0700

    Testing Again Bc of Internal Error on Live Servor Thing

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <h1>Welcome to P1</h1>
 
 <img src = 'http://placekitten.com/200/200'>
+<img src = 'https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-ash4/q71/s720x720/1004686_10153180438190046_880444430_n.jpg>'
