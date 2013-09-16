d38d03b6e5a9592a7c9962f15e66ade6aa864f57
Amira Anuar <amiraanuar@gmail.com>
2013-09-15 20:35:10 -0700

    Added a cSS

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang ="en">
+
+<head>
+<meta charset="utf-8"/>
+<title>Amira's Amazing P1 Project</title>
+<link rel = "stylesheet" type ="text/css" href="index.css"/>
+</head>
+
 <h1>Welcome to P1</h1>
 
 <img src = 'http://placekitten.com/200/200'>'
 
-<h2>testing this out AGAIN</h2>
+<h2>testing this out AGAIN</h2>    
+    
+    
+    
+
+</html>
