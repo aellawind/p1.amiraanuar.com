34d343ba054362417972bc3c90da6752e35df2ac
Amira Anuar <amiraanuar@gmail.com>
2013-09-15 20:50:07 -0700

    CSS change test again

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
 <img src = 'http://placekitten.com/200/200'>'
 
 <h2>testing this out AGAIN</h2>    
+<p> this is an awesome paragraph of awesomeness</p>
+<p>My name: Amira Anuar</p>
+<p>A small bio of myself</p>
+<p>The name of the text editor I am using: Sublime Text 2</p>
     
     
     
