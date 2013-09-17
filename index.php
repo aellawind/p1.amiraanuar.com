0123176c6110cc68903a675b79f0cb85293bc66d
Amira Anuar <amiraanuar@gmail.com>
2013-09-16 19:01:23 -0700

    Changes of background and box

--- index.php
+++ index.php
@@ -7,34 +7,30 @@
 <title>Amira's Amazing P1 Project</title>
 <link rel = "stylesheet" type ="text/css" href="index.css"/>
 
-<style>
-html {background: url(images/jumpingbeach.jpg) no-repeat center center fixed; 
-	-webkit-background-size: cover;
-	-moz-background-size: cover;
-	-o-background-size: cover;
-	background-size: cover;
-}
-</style>
 
 </head>
 
 <body>
-	<div id="page-wrap">
+
+
+<div id="div1">
 <h1>Welcome to P1</h1>
 
 <img src = 'http://placekitten.com/200/200'>'
 
 <h2>testing this out AGAIN</h2>    
-<p> this is an awesome paragraph of awesomeness</p>
-<p>My name: Amira Anuar</p>
+	<p> this is an awesasdfafsdfafsome paragraph of awesomeness</p>
+<p>My name: Amira asdfsafsafaAnuar</p>
 <p>A small bio of myself</p>
 <p>The name of the text editor I am using: Sublime Text 2</p>
-    
+ </div>
+
+
+
 <footer>
 	<p>Copyright 2013 Amira Anuar</p>
 </footer>
 	
-	</div>
 </body>
 
     
