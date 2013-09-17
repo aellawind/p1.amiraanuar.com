b67be2f643b45ef8a9520e7f40595a5d98f98a3f
Amira Anuar <amiraanuar@gmail.com>
2013-09-17 14:40:12 -0700

    Text added and box complete

--- index.php
+++ index.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
 <html lang ="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<title>Amira's Amazing P1 Project</title>
+		<!-- My CSS stylesheet file -->
+		<link rel = "stylesheet" type ="text/css" href="css/index.css"/>
+
+	</head>
+
+	<body>
 
 
-<head>
-<meta charset="utf-8"/>
-<title>Amira's Amazing P1 Project</title>
-<link rel = "stylesheet" type ="text/css" href="css/index.css"/>
+		<div id = "welcome">
+
+		
+			<h1>Welcome to Amira Anuar's P1 Project</h1>
+			<h2>Enjoy the simple CSS3/HTML5!</h2>
+		
+			<img src = "/images/jumpingbeach.JPG" alt="This is a photo of me jumping" width=30% height=30%>
+		
+			<p>My name is Amira, and I'm currently a candidate for the Harvard Extension School 
+				ALM in IT program. I know very basic HTML/CSS (from the old days in middle school
+				when I learned it to customize my MySpace!) so I am eager to really dig in and 
+				learn more. I've bought a book on HTML5/CSS3/Javascript/PHP to help me with this course.
+				Some other fun things about me: I love moving around, so that includes dancing, running, 
+				squash, hiking, tennis, biking, swimming, and skiing. I am a major foodie (who isn't?) and 
+				when I stress out there is nothing more calming than mixing together a batch of brownies. I
+				get very easily absorbed in whatever I'm doing, be it work, playing games, or a tv show.</p>
+
+			<p>My text editor is Sublime, which is awesome!</p>
+		</div>
 
 
-</head>
-
-<body>
-
-
-<div id = "div1">
-<h1>Welcome to P1</h1>
-
-<img src = 'http://placekitten.com/200/200'>'
-<img src = './images/flowers.JPG'>
-<h2>image plztesting this out AGAIN</h2>    
-	<p> this is an awesasdfafsdfafsome paragraph of awesomeness</p>
-<p>My name: Amira aaOMGsdfsafsafaAnuar</p>
-<p>A small bio of myself</p>
-<p>The name of the text editor I am using: Sublime Text 2</p>
-
-</div>
-
-
-<footer>
-	<p>Copyright 2013 Amira Anuar</p>
-</footer>
-	
-</body>
+	</body>
 
     
 
