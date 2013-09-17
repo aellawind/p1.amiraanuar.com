4959e9ffa6b7ddadd709563387bbe4b655990306
Amira Anuar <amiraanuar@gmail.com>
2013-09-17 14:45:05 -0700

    Post validated and fixed image size

--- index.php
+++ index.php
@@ -18,14 +18,14 @@
 			<h1>Welcome to Amira Anuar's P1 Project</h1>
 			<h2>Enjoy the simple CSS3/HTML5!</h2>
 		
-			<img src = "/images/jumpingbeach.JPG" alt="This is a photo of me jumping" width=30% height=30%>
+			<img src = "/images/jumpingbeach.JPG" alt="This is a photo of me jumping" width=160px height=120px>
 		
-			<p>My name is Amira, and I'm currently a candidate for the Harvard Extension School 
+			<p>Hi! I'm Amira, and I'm currently a candidate for the Extension School 
 				ALM in IT program. I know very basic HTML/CSS (from the old days in middle school
 				when I learned it to customize my MySpace!) so I am eager to really dig in and 
 				learn more. I've bought a book on HTML5/CSS3/Javascript/PHP to help me with this course.
 				Some other fun things about me: I love moving around, so that includes dancing, running, 
-				squash, hiking, tennis, biking, swimming, and skiing. I am a major foodie (who isn't?) and 
+				squash, hiking, tennis, biking, swimming, and skiing. I'm a major foodie (who isn't?) and 
 				when I stress out there is nothing more calming than mixing together a batch of brownies. I
 				get very easily absorbed in whatever I'm doing, be it work, playing games, or a tv show.</p>
 
